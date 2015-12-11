ff9332a23a429c89e026397011046cc21fdcf1aa
Gabe Fernando <gef756@gmail.com>
2015-12-11 15:32:00 -0500

    ENH: running server

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
   MEAN Tutorial
   from https://thinkster.io/mean-stack-tutorial
 */
+
+'use strict';
+
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', ['$scope', function($scope) {
+  $scope.test = 'Hello world!';
+}]);
