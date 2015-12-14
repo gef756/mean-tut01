5e15d64ceba61e4285b71f8b890739f028264f36
Gabe Fernando <gef756@gmail.com>
2015-12-14 08:01:24 -0500

    ENH: allow user to add and view links for posts

--- app.js
+++ app.js
@@ -22,8 +22,14 @@ app.controller('MainCtrl', ['$scope', function($scope) {
     if(!$scope.title || $scope.title === '') {
       return;
     }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
+    // Reset input for next submission
     $scope.title = '';
+    $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
