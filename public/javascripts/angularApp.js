f438ec19bebfb2c045a3850cd2237d3a3a85960c
Gabe Fernando <gef756@gmail.com>
2015-12-18 23:24:23 -0500

    ENH: set up authentication controller

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -194,3 +194,31 @@ app.controller('PostsCtrl', [
     };
   }
 ]);
+
+app.controller('AuthCtrl', ['$scope', '$state', 'auth',
+  function($scope, $state, auth) {
+    $scope.user = {};
+
+    $scope.register = function() {
+      auth
+        .register($scope.user)
+        .error(function(error) {
+          $scope.error = error;
+        })
+        .then(function() {
+          $scope.go('home');
+        });
+    };
+
+    $scope.logIn = function() {
+      auth
+        .logIn($scope.user)
+        .error(function(error) {
+          $scope.error = error;
+        })
+        .then(function() {
+          $state.go('home');
+        });
+    };
+  };
+]);
