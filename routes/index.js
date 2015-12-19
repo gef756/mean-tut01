da04546f568422e74af7bd67d9c5d42a7ea1b80f
Gabe Fernando <gef756@gmail.com>
2015-12-18 21:59:18 -0500

    ENH: add login endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 var router = express.Router();
 
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -116,6 +118,22 @@ router.post('/register', function(req, res, next) {
     }
     return res.json({token: user.generateJWT()});
   });
+});
+
+router.post('/login', function(req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
